6f1ffd95edb9c33e6d5b813cd719ffa7d5fc6bbc
Brayan Sierra <sbrayandres@gmail.com>
2023-10-24 19:46:18 -0500

    se instala admin lte y se modifica

--- resources/views/etiquetas/index.blade.php
+++ resources/views/etiquetas/index.blade.php
@@ -1,3 +1,59 @@
-@extends('layouts.app')
+@extends('adminlte::page')
 
-@endsection
+@section('App Galery')
+
+@section('content_header')
+    <div class="d-flex justify-content-between">
+        <h1>App Galery</h1>
+        <a class="btn btn-secondary" href="{{ route('etiqueta.create') }}">Crear Etiqueta</a>
+    </div>
+@stop
+@section('content')
+    @if (session('info'))
+        <div class="alert alert-success">
+            <strong>{{ session('info') }}</strong>
+
+        </div>
+    @endif
+
+    <div class="card-body">
+
+        <table class="table table-striped">
+
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th colspan="2"></th>
+                </tr>
+
+            </thead>
+            @foreach ($etiquetas as $etiqueta)
+                <tr>
+                    <td>{{ $etiqueta->id }}</td>
+                    <td>{{ $etiqueta->title }}</td>
+                    <td width="10px">
+                        <a class="btn btn-primary btn-sm" href="{{ route('etiqueta.edit', $etiqueta) }}">Editar</a>
+                    </td>
+                    <td width="10px">
+                        <form method="POST" action="{{ route('etiqueta.destroy', $etiqueta) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+
+            <tbody>
+
+
+            </tbody>
+
+        </table>
+
+    </div>
+
+    </div>
+
+@stop
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,10 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Etiquetas') }}</div>
 
+                    <div class="card-header">{{ __('Etiquetas') }}</div>
+                    <div>
+                    </div>
                     <div class="card-body">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
